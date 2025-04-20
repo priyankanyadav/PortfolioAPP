76344a900013d78da15814fcba6f108e62603ca1
priyanka <priyankanyadav1144@gmail.com>
2025-04-19 18:39:47 -0700

    Add project files.

---
+++ PortfolioAPP/Pages/Experience.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PortfolioAPP.Pages
+{
+    public class ExperienceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
